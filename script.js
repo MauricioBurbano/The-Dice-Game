8239f53db527f3ed08ff1a6667302024cb8566d7
mauri <morocobt@gmail.com>
2023-09-06 17:43:11 -0400

    add dice images

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 var message = document.querySelector("h1");
+var diceOneImage = document.querySelector("#one");
+var diceTwoImage = document.querySelector("#two");
 
 if (!sessionStorage.getItem("refresh")) {
     sessionStorage.setItem("refresh", "true");
     sessionStorage.setItem("draw", "true");
     message.innerText = "Refresh Me";
+    setImage(diceOneImage, 6);
+    setImage(diceTwoImage, 6);
 } else {
     if (sessionStorage.getItem("draw") === "false") {
         sessionStorage.setItem("draw", "true");
         message.innerText = "Draw!"
+        setImage(diceOneImage, 1);
+        setImage(diceTwoImage, 1);
     } else {
         sessionStorage.setItem("draw", "false");
         draw();
@@ -18,11 +24,19 @@ function draw() {
     var diceOne = Math.ceil(Math.random() * 6);
     var diceTwo = Math.ceil(Math.random() * 6);
 
-    if (diceOne > diceTwo) {
-        message.innerText = "Player One Wins!";
-    } else if (diceOne < diceTwo) {
-        message.innerText = "Player Two Wins!";
-    } else {
+    if (diceOne === diceTwo) {
         draw();
+    } else {
+        setImage(diceOneImage, diceOne);
+        setImage(diceTwoImage, diceTwo);
+        if (diceOne > diceTwo) {
+            message.innerText = "Player 1 Wins!";
+        } else {
+            message.innerText = "Player 2 Wins!";
+        }
     }
+}
+
+function setImage(dice, face) {
+    dice.src = "images/dice" + face + ".png";
 }
